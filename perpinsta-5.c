6f39bde794ca53cb8ed061abacc441fb5d413572
Harsh Jiyani <harshjiyani4277@gmail.com>
2025-01-12 21:42:41 +0530

    Add files via upload

---
+++ perpinsta-5.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+//main program
+int main ()
+{
+  //variables initialization
+  int num, reverse = 0, rem;
+  printf("enter the number");
+  scanf("%d",&num);
+  printf("The number is: %d\n",num);
+  
+ 
+  
+    while(num != 0)
+    {
+      rem = num % 10;
+      reverse = reverse * 10 + rem;
+      num /= 10;
+    };
+ 
+  
+  printf("Reverse: %d\n",reverse);
+  
+  return 0;
+}
